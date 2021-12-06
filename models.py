0c0e17de38c99c0c72860076c4807ca65cfc5e90
Dan <serendipitypwnz@gmail.com>
2021-12-06 23:31:01 +0200

    spent whole day reading papers

--- models.py
+++ models.py
@@ -2,9 +2,17 @@ import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
 
+# https://arxiv.org/pdf/1508.04025.pdf
+
 class ModelV1(pl.LightningModule):
-    def __init__(self):
+    def __init__(self, input_size, hidden_size, num_layers=1, dropout=0):
         super().__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.input_size = hidden_size
+
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, dropout, bidirectional=True)
+        
 
     def forward(self, x):
         pass
